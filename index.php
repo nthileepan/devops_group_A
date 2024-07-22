ddc43d46f6a419408b02978d4fe74bcda070226b
nthileepan <thileepanraja03@gmail.com>
2024-07-22 15:02:36 +0530

    second commit

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
 <!-- Hero End -->
 
 
-<!-- JavaScript Libraries -->
+<!-- JavaScript Libraries ok-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
